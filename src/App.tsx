b6cfd5d0c5a4a52ad6ae63df1248ed5dbd59c9f6
Giovani Silva <giovani.silva@live.com>
2022-07-28 21:42:13 -0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PlusCircle } from 'phosphor-react';
+import { ClipboardText, PlusCircle, Trash } from 'phosphor-react';
 import styles from './App.module.css';
 import { Header } from './components/Header';
 
@@ -12,6 +12,37 @@ export function App() {
           Criar <PlusCircle size={24} />
         </button>
       </form>
+      <nav className={styles.nav}>
+        <span>
+          Tarefas criadas <small>0</small>
+        </span>
+        <span>
+          Concluídas <small>0</small>
+        </span>
+      </nav>
+
+      <div className={styles.empty}>
+        <ClipboardText size={56} />
+        <strong>Você ainda não tem tarefas cadastradas</strong>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </div>
+
+      <ul className={styles.toDoList}>
+        {[1, 2, 3, 4, 5].map((item) => {
+          return (
+            <li key="item">
+              <input className={styles.checkInput} type="checkbox" id={'check-' + item} />
+              <label className={styles.checkLabel} htmlFor={'check-' + item}></label>
+              <span className={styles.task}>
+                Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+              </span>
+              <button className={styles.delete} title="delete">
+                <Trash size={18} />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
